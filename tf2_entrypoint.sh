9f845d3871db96f346fcc4073831e6f1d2b076b2
mathieuduperre <mathieu.duperre@gmail.com>
2019-03-29 08:04:47 -0400

    Update tf2_entrypoint.sh

--- tf2_entrypoint.sh
+++ tf2_entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 trap '' TERM INT HUP
+./steamcmd.sh +login anonymous +force_install_dir ./csgo +app_update 232250 validate +quit
 
 cd tf2
 ./srcds_run -game tf -console +sv_pure 1 +randommap +maxplayers 24
